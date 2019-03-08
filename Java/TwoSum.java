b44d13166c2b7ccda5f5c3767aa7c4be03cd478b
HuXiaoPan <hupan008@qq.com>
2019-03-08 21:08:47 +0800

    0001

---
+++ Java/TwoSum.java
@@ -0,0 +1,23 @@
+package TwoSum;
+
+import java.util.HashMap;
+
+/**
+ * @author hupan
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ */
+public class TwoSum {
+	
+	public int[] GetTwoSum(int[] nums,int target) {
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			int implement = target - nums[i];
+			if (map.containsKey(implement)) {
+				return new int[] {map.get(implement),i};
+			}
+			map.put(nums[i], i);
+		}
+		return null;
+	}
+}
